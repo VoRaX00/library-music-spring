e91967032623ef18a626002521f4f8f19f332c19
nikita <nikitakerzhik@gmail.com>
2025-03-22 00:50:24 +1000

    init project

---
+++ src/main/java/org/example/librarymusic/LibraryMusicApplication.java
@@ -0,0 +1,13 @@
+package org.example.librarymusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryMusicApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/librarymusic/controllers/MusicController.java
@@ -0,0 +1,7 @@
+package org.example.librarymusic.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MusicController {
+}
